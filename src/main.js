a74df8ce53b544da875249d59528372061f522dd
Jie <jzhu@tripadvisor.com>
2016-09-08 11:12:41 -0400

    add a status bar icon

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
-const {app, BrowserWindow, Tray} = require('electron')
+const {app, BrowserWindow, nativeImage, Tray} = require('electron');
+const path = require('path');
 
 app.on("ready", () => {
-    let tray = new Tray();
+    let image = nativeImage.createFromPath(path.join(__dirname, '../assets/status_bar_linux.png'));
+    let tray = new Tray(image);
     let win = new BrowserWindow({width: 800, height: 600, frame: false});
 
     // menubar.window.loadURL(`file://${__dirname}/index.html`);
-    const path = `file://${__dirname}/clipboard.html`;
+    const contentPath = `file://${__dirname}/clipboard.html`;
     win.on('close', function () { win = null })
-    win.loadURL(path);
+    win.loadURL(contentPath);
 
     // Menubar onclick event listener
     tray.on('click', () => {
